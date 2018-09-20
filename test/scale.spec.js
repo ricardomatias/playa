7b3d5fa6be866037d51ad0708c340f5741ecd92e
Ricardo Matias <ricardo.matias0@gmail.com>
2018-09-20 13:08:31 +0200

    I can use a Scale

--- test/scale.spec.js
+++ test/scale.spec.js
@@ -1,95 +1,97 @@
-const Scale = require('../lib/scale');
+const Scale = require('../lib/Scale');
 
-const scale = new Scale();
+const {
+  CHROMATIC,
+  MAJOR,
+  MINOR,
+  LYDIAN,
+  MINOR_PENTATONIC,
+  MAJOR_PENTATONIC
+} = require('../lib/scales');
 
-describe('Scale Test Suite', t => {
+describe('Scale Test Suite', () => {
 
-  describe('#_organizeNotes', () => {
+  describe('#_createScale', () => {
 
-    it('should organize notes - natural', () => {
-      const notes = scale._organizeNotes('D');
+    it('should create scale - chromatic', () => {
+      const scale = new Scale('C', Scale.CHROMATIC);
 
-      expect(notes).to.eql(['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#']);
+      expect(scale.nStr).to.eql(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']);
     });
-
-    it('should organize notes - sharp', () => {
-      const notes = scale._organizeNotes('F#');
-
-      expect(notes).to.eql(['F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F']);
-    });
-
-    it('should organize notes - flat', () => {
-      const notes = scale._organizeNotes('Bb');
-
-      expect(notes).to.eql(['Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A']);
-    });
-  });
-
-  describe('#createScale', () => {
 
     it('should create scale - natural', () => {
-      const notes = scale.createScale('C', scale.MAJOR);
-
-      expect(notes).to.eql(['C', 'D', 'E', 'F', 'G', 'A', 'B']);
+      const scale = new Scale('C', Scale.MAJOR);
+      
+      expect(scale.nStr).to.eql(['C', 'D', 'E', 'F', 'G', 'A', 'B']);
     });
 
     it('should create scale - sharp', () => {
-      const notes = scale.createScale('G#', 'minor');
+      const scale = new Scale('G#', Scale.MINOR);
 
-      expect(notes).to.eql(['G#', 'A#', 'B', 'C#', 'D#', 'E', 'F#']);
+      expect(scale.nStr).to.eql(['G#', 'A#', 'B', 'C#', 'D#', 'E', 'F#']);
     });
 
     it('should create scale - flat', () => {
-      const notes = scale.createScale('Db', 'lydian');
+      const scale = new Scale('Db', Scale.LYDIAN);
 
-      expect(notes).to.eql([ 'Db', 'Eb', 'F', 'G', 'Ab', 'Bb', 'C' ]);
+      expect(scale.nStr).to.eql([ 'Db', 'Eb', 'F', 'G', 'Ab', 'Bb', 'C' ]);
+    });
+
+    it('should create scale - pentatonic', () => {
+      const scale = new Scale('C', Scale.MINOR_PENTATONIC);
+
+      expect(scale.nStr).to.eql([ 'C', 'D#', 'F', 'G', 'A#' ]);
     });
   });
 
   describe('#mapScaleOctaves', () => {
 
-    it('should create scale - natural', () => {
-      const notes = scale.createScale('C', scale.MAJOR);
-      const octaves = scale.mapScaleOctaves(notes);
+    it('should map - natural', () => {
+      const scale = new Scale('C', Scale.MAJOR);
+      const octaves = scale.mapScaleOctaves();
 
-      expect(octaves[0]).to.eql('C0');
-      expect(octaves[30]).to.eql('E5');
-      expect(octaves[octaves.length - 1]).to.eql('B9');
-    });
+      const half = Math.round(octaves.length / 2);
 
-    it('should create scale - sharp', () => {
-      const notes = scale.createScale('E', scale.MAJOR);
-      const octaves = scale.mapScaleOctaves(notes);
-
-      expect(octaves[0]).to.eql('E0');
-      expect(octaves[30]).to.eql('G#5');
-      expect(octaves[octaves.length - 1]).to.eql('D#9');
-    });
-
-    it('should create scale - flat', () => {
-      const notes = scale.createScale('Ab', scale.MAJOR);
-      const octaves = scale.mapScaleOctaves(notes);
-
-      expect(octaves[0]).to.eql('Ab0');
-      expect(octaves[30]).to.eql('C5');
+      expect(octaves[0]).to.eql('C-1');
+      expect(octaves[half]).to.eql('F4');
       expect(octaves[octaves.length - 1]).to.eql('G9');
+      expect(octaves).to.have.length(75);
     });
-  });
 
-  describe('#modulate', () => {
-    
-    it('should modulate UP', () => {
-      const notes = scale.createScale('C', scale.MAJOR);
-      const newScale = scale.modulate(notes, scale.MAJOR, scale.UP);
+    it('should map - sharp', () => {
+      const scale = new Scale('E', Scale.MAJOR);
+      const octaves = scale.mapScaleOctaves();
 
-      expect(newScale).to.eql(['G', 'A', 'B', 'C', 'D', 'E', 'F#']);
-    })
+      const half = Math.round(octaves.length / 2);
 
-    it('should modulate DOWN', () => {
-      const notes = scale.createScale('A', scale.MAJOR);
-      const newScale = scale.modulate(notes, scale.MAJOR, scale.DOWN);
+      expect(octaves[0]).to.eql('E-1');
+      expect(octaves[half]).to.eql('F#4');
+      expect(octaves[octaves.length - 1]).to.eql('F#9');
+      expect(octaves).to.have.length(72);
+    });
 
-      expect(newScale).to.eql(['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
-    })
+    it('should map - flat', () => {
+      const scale = new Scale('Ab', Scale.MAJOR);
+      const octaves = scale.mapScaleOctaves();
+
+      const half = Math.round(octaves.length / 2);
+
+      expect(octaves[0]).to.eql('Ab-1');
+      expect(octaves[half]).to.eql('Ab4');
+      expect(octaves[octaves.length - 1]).to.eql('G9');
+      expect(octaves).to.have.length(70);
+    });
+
+    it('should map - pentatonic', () => {
+      const scale = new Scale('Eb', Scale.MAJOR_PENTATONIC);
+      const octaves = scale.mapScaleOctaves();
+
+      const half = Math.round(octaves.length / 2);
+
+      expect(octaves[0]).to.eql('Eb-1');
+      expect(octaves[half]).to.eql('G4');
+      expect(octaves[octaves.length - 1]).to.eql('G9');
+      expect(octaves).to.have.length(53);
+    });
   });
 });
