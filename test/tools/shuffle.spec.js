09ff551ee55edd8f598ca441a7926337ba80ef5c
Ricardo Matias <ricardo.matias0@gmail.com>
2018-10-19 22:23:13 +0200

    Adapt Shuffle to use Simplex noise

--- test/tools/shuffle.spec.js
+++ test/tools/shuffle.spec.js
@@ -1,9 +1,12 @@
+import { seedRandom } from '../../lib/tools/random';
 import { shuffle } from '../../lib/tools';
 
 const AM7 = [ 'A', 'C', 'E', 'G', 'B', 'Eb' ];
 
 describe('A Shuffle test suite', () => {
 	it('should shuffle the array', () => {
+		seedRandom('test');
+
 		for (let index = 0; index < 25; index++) {
 			shuffle(AM7);
 
