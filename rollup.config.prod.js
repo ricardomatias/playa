10201c3551b0b520e7b909d48f6ea28c45644b42
Ricardo Matias <ricardo.matias0@gmail.com>
2020-01-30 19:14:49 +0100

    Add docs and TS types

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,115 +2,110 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import cleanup from 'rollup-plugin-cleanup';
-import { terser } from 'rollup-plugin-terser';
+import del from 'rollup-plugin-delete';
+// import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
+import progress from 'rollup-plugin-progress';
 // import includePaths from 'rollup-plugin-includepaths';
-
-
-import { lstatSync, readdirSync } from 'fs';
-import { join } from 'path';
-
-const ignoredFolders = new RegExp(/utils|core/gi);
-
-const removeExt = (file) => file.replace(/\.[^.]+$/, '');
-
-const isDirectory = (source) => lstatSync(source).isDirectory();
-
-const getIndex = (dir) =>
-	join(dir, readdirSync(dir).find((file) => /^index/.test(file)));
-
-const getFiles = (dir) =>
-	readdirSync(dir)
-		.filter((file) => !/^_/.test(file))
-		.reduce((acc, file) => {
-			const path = join(dir, file);
-
-			if (ignoredFolders.test(path)) {
-				return acc;
-			}
-
-			try {
-				const finalPath = isDirectory(path) ? getIndex(path) : path;
-				return {
-					...acc,
-					[removeExt(file)]: finalPath,
-				};
-			} catch (e) {
-				console.log(dir, path);
-				console.error(e);
-				return acc;
-			}
-		}, {});
-
-const publicFiles = getFiles(join(__dirname, 'lib'));
-
-const includePathOptions = {
-	include: {},
-	paths: [ 'lib/core', 'lib/functional', 'lib/tools', 'lib/utils' ],
-	external: [],
-	extensions: [ '.js' ],
-};
 
 export default [
 	{
 		input: {
 			'playa': 'lib/index.js',
-			'playa/core': 'lib/core/index.js',
-			'playa/functional': 'lib/functional/index.js',
-			'playa/tools': 'lib/tools/index.js',
-			'playa/constants': 'lib/constants/index.js',
-			'playa/utils': 'lib/utils/index.js',
+			'core': 'lib/core/index.js',
+			'functional': 'lib/functional/index.js',
+			'tools': 'lib/tools/index.js',
+			'constants': 'lib/constants/index.js',
+			'utils': 'lib/utils/index.js',
+		},
+		output: {
+			dir: '../playa-release',
+			entryFileNames: '[name].js',
+			format: 'esm',
+			// sourcemap: true,
+		},
+		manualChunks: {
+			ramda: [ 'ramda' ],
+		},
+		plugins: [
+			del({ targets: 'build/*' }),
+			resolve(),
+			commonjs(),
+			cleanup({
+				comments: 'jsdoc',
+				compactComments: false,
+			}),
+			// terser({
+			// 	output: {
+			// 		comments: 'all',
+			// 	},
+			// }),
+			// includePaths(includePathOptions),
+			copy({
+				targets: [
+					{ src: 'types/*.ts', dest: '../playa-release' },
+				],
+			}),
+			progress(),
+		],
+	},
+	{
+		input: {
+			'playa': 'lib/index.js',
 		},
 		output: [
 			{
-				dir: 'build',
-				entryFileNames: '[name].esm.js',
-				format: 'es',
-				sourcemap: true,
-			},
-			{
-				dir: 'build',
-				entryFileNames: '[name].cjs.js',
+				dir: 'build/cjs',
+				entryFileNames: '[name].js',
 				format: 'cjs',
 				sourcemap: true,
 			},
 		],
-		plugins: [
-			cleanup(),
-			resolve(),
-			commonjs(),
-			babel({
-				exclude: 'node_modules/**',
-			}),
-			terser({
-				output: {
-					comments: 'all',
-				},
-			}),
-			// includePaths(includePathOptions),
-		],
-	},
-	{
-		input: 'lib/index.js',
-		output: {
-			dir: 'build',
-			input: '[name].umd.js',
-			format: 'umd',
-			name: 'playa',
-			sourcemap: true,
+		manualChunks: {
+			ramda: [ 'ramda' ],
 		},
 		plugins: [
-			cleanup(),
+			del({ targets: 'build/*' }),
 			resolve(),
 			commonjs(),
+			cleanup({
+				comments: 'jsdoc',
+				compactComments: false,
+			}),
 			babel({
 				exclude: 'node_modules/**',
 			}),
-			terser({
-				output: {
-					comments: 'all',
-				},
-			}),
+			// terser({
+			// 	output: {
+			// 		comments: 'all',
+			// 	},
+			// }),
 			// includePaths(includePathOptions),
+			progress(),
 		],
 	},
+	// {
+	// 	input: 'lib/index.js',
+	// 	output: {
+	// 		dir: 'build',
+	// 		input: '[name].umd.js',
+	// 		format: 'umd',
+	// 		name: 'playa',
+	// 		sourcemap: true,
+	// 	},
+	// 	plugins: [
+	// 		cleanup(),
+	// 		resolve(),
+	// 		commonjs(),
+	// 		babel({
+	// 			exclude: 'node_modules/**',
+	// 		}),
+	// 		terser({
+	// 			output: {
+	// 				comments: 'all',
+	// 			},
+	// 		}),
+	// 		// includePaths(includePathOptions),
+	// 	],
+	// },
 ];
