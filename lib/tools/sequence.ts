260932ef095bf2abecb47de0f89177903e114431
Ricardo Matias <ricardo.matias0@gmail.com>
2020-02-24 11:26:16 +0100

    Add Sequence tool

---
+++ lib/tools/sequence.ts
@@ -0,0 +1,119 @@
+import * as R from 'ramda';
+import { Scale, Key, Chord } from '../core';
+import distance from './distance';
+import { mapNotes } from '../utils';
+import NoteType from '../core/types';
+
+
+/**
+ * Defines a sequence of notes/chords
+ * @class
+ * @memberof Tools
+ *
+ * @name Sequence
+ */
+class Sequence {
+	private _scale: Scale;
+	private _chords: Chord[] = []
+	private _notes = []
+	private _canCreateChords = false;
+	private _intervals: string[];
+
+	/**
+	* Creates an instance of Sequence.
+	* @constructor
+	* @memberof Tools
+	*
+	* @param {Scale | Key} scale
+	*/
+	constructor(scale: Scale) {
+		if (scale instanceof Key) {
+			this._canCreateChords = true;
+		}
+
+		this._scale = scale;
+		this._scale = scale;
+		this._intervals = scale.type.split(' ');
+	}
+
+	get string() {
+		if (!this._notes.length) {
+			return [];
+		}
+
+		const notes = R.sortBy(R.prop('midi'), this._notes);
+
+		return mapNotes(notes, NoteType.STR);
+	}
+
+	get midi() {
+		if (!this._notes.length) {
+			return [];
+		}
+
+		return mapNotes(this._notes, NoteType.MIDI);
+	}
+
+	get freq() {
+		if (!this._notes.length) {
+			return [];
+		}
+
+		return mapNotes(this._notes, NoteType.FREQ);
+	}
+
+	get chords() {
+		const chords = this._chords;
+
+		return {
+			array: chords,
+			midi: chords.map((chord) => chord.midi),
+			string: chords.map((chord) => chord.name),
+			freq: chords.map((chord) => chord.freq),
+		};
+	}
+
+	private getAtPosition(reg: RegExp, romanPos: string) {
+		const scale = this._scale;
+		const root = scale.root;
+		const interval = this._intervals.find((interval) => (reg.test(interval)));
+
+		if (!interval) return this;
+
+		const notes = scale.notes.filter((note) => (note.note === distance.transposeUp(root, interval)));
+
+		this._notes = this._notes.concat(notes);
+
+		if (this._canCreateChords) {
+			const chord = scale[romanPos].chord;
+
+			this._chords.push(chord);
+		}
+
+		return this;
+	}
+
+	get I() {
+		return this.getAtPosition(/1/, 'I');
+	}
+	get II() {
+		return this.getAtPosition(/2/, 'II');
+	}
+	get III() {
+		return this.getAtPosition(/3/, 'III');
+	}
+	get IV() {
+		return this.getAtPosition(/4/, 'IV');
+	}
+	get V() {
+		return this.getAtPosition(/5/, 'V');
+	}
+	get VI() {
+		return this.getAtPosition(/6/, 'VI');
+	}
+	get VII() {
+		return this.getAtPosition(/7/, 'VII');
+	}
+}
+
+export default Sequence;
---
+++ test/tools/sequence.spec.js
@@ -0,0 +1,90 @@
+import { seedRandom } from '../../lib/tools/random';
+import { Sequence } from '../../lib/tools';
+import { Key, Chord, Scale } from '../../lib/core';
+
+describe('A Sequence test suite', () => {
+	it('should create a sequence of notes', () => {
+		seedRandom('test');
+
+		const scale = new Scale('A', Scale.MAJOR);
+
+		const seq = new Sequence(scale).I.II.V.string;
+
+		expect(seq).toEqual([ 'A3', 'B3', 'E4' ]);
+	});
+
+	it('should create a sequence of notes with 2 octaves', () => {
+		seedRandom('test');
+
+		const scale = new Scale('A', Scale.MAJOR, [ 3, 2 ]);
+
+		const seq = new Sequence(scale).II.V.VI.string;
+
+		expect(seq).toEqual([ 'B3', 'E4', 'F#4', 'B4', 'E5', 'F#5' ]);
+	});
+
+	it('should create a sequence of chords', () => {
+		seedRandom('test');
+
+		const key = new Key('A', Key.MAJOR, [ 3, 1 ], { chordStructure: Chord.TRIAD });
+
+		const chords = new Sequence(key).II.V.VI.chords;
+
+		expect(chords.string).toEqual([ 'Bm', 'Emaj', 'F#m' ]);
+		expect(chords.midi).toEqual([ [ 71, 74, 78 ], [ 64, 68, 71 ], [ 66, 69, 73 ] ]);
+	});
+
+	it('should create a way too long sequence', () => {
+		seedRandom('test');
+
+		const key = new Key('A', Key.MAJOR, [ 3, 2 ], { chordStructure: Chord.SEVENTH });
+
+		const chords = new Sequence(key).II.I.V.VI.V.VI.I.III.VII.chords;
+
+		expect(chords.string).toEqual([
+			'Bm7', 'AM7',
+			'E7', 'F#m7',
+			'E7', 'F#m7',
+			'AM7', 'C#m7',
+			'G#m7b5',
+		]);
+		expect(chords.midi).toEqual([
+			[
+				71, 74, 78, 81,
+				83, 86, 90, 93,
+			],
+			[
+				69, 73, 76, 80,
+				81, 85, 88, 92,
+			],
+			[
+				64, 68, 71, 74,
+				76, 80, 83, 86,
+			],
+			[
+				66, 69, 73, 76,
+				78, 81, 85, 88,
+			],
+			[
+				64, 68, 71, 74,
+				76, 80, 83, 86,
+			],
+			[
+				66, 69, 73, 76,
+				78, 81, 85, 88,
+			],
+			[
+				69, 73, 76, 80,
+				81, 85, 88, 92,
+			],
+			[
+				61, 64, 68, 71,
+				73, 76, 80, 83,
+			],
+			[
+				68, 71, 74, 78,
+				80, 83, 86, 90,
+			],
+		]);
+	});
+});
