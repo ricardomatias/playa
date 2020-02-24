66fa26ff1faf266cea17493a7459504431bba3e1
Ricardo Matias <ricardo.matias0@gmail.com>
2020-02-24 11:39:54 +0100

    Improve choose with comparison and picking multiple elements

--- test/tools/choose.spec.js
+++ test/tools/choose.spec.js
@@ -7,10 +7,21 @@ describe('A Choose test suite', () => {
 	beforeAll(() => seedRandom('CHOOSE'));
 
 	it('should return a random element', () => {
-		expect(choose(AM7)).toBe('E');
 		expect(choose(AM7)).toBe('C');
 		expect(choose(AM7)).toBe('E');
-		expect(choose(AM7)).toBe('E');
-		expect(choose(AM7)).toBe('G');
+	});
+
+	it('should return 2 random elements', () => {
+		const [ first, second ] = choose(AM7, 2);
+
+		expect(first).toBe('E');
+		expect(second).toBe('G');
+	});
+
+	it('should return 2 random elements but never a C', () => {
+		const [ first, second ] = choose(AM7, 2, 'C');
+
+		expect(first).toBe('E');
+		expect(second).toBe('A');
 	});
 });
