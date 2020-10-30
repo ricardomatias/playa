da54a2f16791af0d7b10e230bbe99a01a00a3f9d
Ricardo Matias <ricardo.matias0@gmail.com>
2020-10-30 11:45:54 +0100

    Add Rhythm Presets to docs
    Closes #33

--- lib/composition/rhythm/presets.ts
+++ lib/composition/rhythm/presets.ts
@@ -1,10 +1,54 @@
 import { Notevalue } from '../../constants';
 
+
+/**
+ * Rhythm Presets
+ * @memberof Tools.Rhythm
+ * @typedef {Notevalue[]} Presets
+ * @enum
+ * @example
+ * Rhythm.Presets.Robotic => [ '8n', '16n', '32n' ]
+ */
 export const Presets = <const>{
+	/**
+	* Mixed
+	* @type {Notevalue[]}
+	* @memberof Tools.Rhythm.Presets
+	* @default
+	* @readonly
+	*/
 	Mixed: <Notevalue[]>[ '4n', '8n', '4nt', '4nd', '8nt', '8nd', '16n' ],
+	/**
+	* Slow
+	* @type {Notevalue[]}
+	* @memberof Tools.Rhythm.Presets
+	* @default
+	* @readonly
+	*/
 	Slow: <Notevalue[]>[ '2n', '4nd', '2nd', '4n' ],
+	/**
+	* Robotic
+	* @type {Notevalue[]}
+	* @memberof Tools.Rhythm.Presets
+	* @default
+	* @readonly
+	*/
 	Robotic: <Notevalue[]>[ '8n', '16n', '32n' ],
+	/**
+	* Straight
+	* @type {Notevalue[]}
+	* @memberof Tools.Rhythm.Presets
+	* @default
+	* @readonly
+	*/
 	Straight: <Notevalue[]>[ '2n', '4n', '8n', '16n' ],
+	/**
+	* Common
+	* @type {Notevalue[]}
+	* @memberof Tools.Rhythm.Presets
+	* @default
+	* @readonly
+	*/
 	Common: <Notevalue[]>[ '4n', '8n', '16n', '4nt', '8nt', '4nd' ],
 };
 
